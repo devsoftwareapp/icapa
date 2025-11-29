ab4ee20a2108d8fb4e3465c5c03556c770e5fbde
devsoftwareapp <devsoftwaremail@gmail.com>
2025-11-30 00:08:44 +0300

    Update app_languages.dart

--- lib/app_languages.dart
+++ lib/app_languages.dart
@@ -1,4 +1,5 @@
 // lib/app_languages.dart
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
